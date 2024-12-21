481604ed0a1515531d741fb9913220107983c612
Siam <siamekanto360@gmail.com>
2024-12-21 14:11:58 +0600

    add suspense boundary in products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,11 @@
+"use client";
 import { Products } from "@/views/products";
+import { Suspense } from "react";
 
 export default function ProductsRoot() {
-  return <Products />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Products />
+    </Suspense>
+  );
 }
